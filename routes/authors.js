cbb4ee6b1f937c8dbfa41b9f2d70a94ad88742c8
StasPromist <ctacbars@gmail.com>
2023-07-27 22:33:26 +0300

    Added new functionality

--- routes/authors.js
+++ routes/authors.js
@@ -3,17 +3,17 @@ const router = express.Router()
 const Author = require('../models/author')
 
 // All Authors Route
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
     let searchOptions = {}
     if (req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
-    try{
+    try {
         const authors = await Author.find(searchOptions)
-        res.render('authors/index', {authors: authors, searchOptions: req.query})
+        res.render('authors/index', { authors: authors, searchOptions: req.query })
     } catch {
-     res.redirect('/')
-    }  
+        res.redirect('/')
+    }
 })
 
 // New Author Route
@@ -27,15 +27,67 @@ router.post('/', async (req, res) => {
         name: req.body.name
     })
     try {
-       const newAuthor = await author.save()
-            res.redirect(`authors`)
-            // res.redirect(`authors/${newAuthor.id}`)
-    } 
+        const newAuthor = await author.save()
+        res.redirect(`authors/${newAuthor.id}`)
+    }
     catch {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating Author'
         })
-    }})
+    }
+})
+
+router.get('/:id', (req, res) => {
+    res.send('Show Author' + req.params.id)
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', { author: author })
+    } catch {
+        res.redirect('/authors')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    }
+    catch {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors')
+    }
+    catch {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+            
+        }
+
+    }
+})
 
 module.exports = router
